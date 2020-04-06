2a35e447542adb966fbace86f0a07fdfc77089aa
Roberto Cipollone <cipollone.rt@gmail.com>
2020-04-06 17:15:17 +0200

    tools and dependencies updated.
    + Dependencies: removed keras, added tensorflow 2.1. Planning to
      use tf.keras.
    + Fix in package version and added Pipfile

--- setup.py
+++ setup.py
@@ -3,15 +3,13 @@ from setuptools import find_packages
 
 
 setup(name='keras-rl',
-      version='0.4.0',
+      version='0.4.2',
       description='Deep Reinforcement Learning for Keras',
       author='Matthias Plappert',
       author_email='matthiasplappert@me.com',
       url='https://github.com/keras-rl/keras-rl',
       download_url='https://github.com/keras-rl/keras-rl/archive/v0.4.0.tar.gz',
       license='MIT',
-      install_requires=['keras>=2.0.7'],
-      extras_require={
-          'gym': ['gym'],
-      },
+      install_requires=['numpy', 'gym[atari]', 'tensorflow~=2.1'],
+      extras_require={},
       packages=find_packages())
